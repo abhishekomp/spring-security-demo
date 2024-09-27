670b2bd36014053c99df3761e2bb20cbe871c4fa
abhishekomp <abhishek2283@hotmail.com>
2024-09-27 12:02:13 +0200

    add user via InMemoryUserDetailsManager and then access the endpoint

--- src/main/java/org/aom/greeting/config/SecurityAppConfig.java
+++ src/main/java/org/aom/greeting/config/SecurityAppConfig.java
@@ -1,11 +1,43 @@
 package org.aom.greeting.config;
 
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity (debug = true)
 // this annotation will create the SpringSecurityFilterChain that will by default protect all our end points and also show the login page in the browser when we try to access our end point.
 public class SecurityAppConfig {
-
-}
+	
+	@Bean
+	InMemoryUserDetailsManager setUpUsers() {
+		
+		GrantedAuthority role1 = new SimpleGrantedAuthority("user");
+		GrantedAuthority role2 = new SimpleGrantedAuthority("admin");
+		
+		ArrayList<GrantedAuthority> authoritiesList = new ArrayList<>();
+		
+		UserDetails john = new User("john", "john", authoritiesList);
+		
+		InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+		userDetailsManager.createUser(john);
+		
+		return userDetailsManager;
+		
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Bean
+	PasswordEncoder noOpPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+} 
